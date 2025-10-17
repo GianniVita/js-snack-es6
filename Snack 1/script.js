dc92a2ae8b6bf9851a3a0cd88ebf7acd3c97cf18
giannivitale <giavitale78@gmail.com>
2025-10-17 15:43:48 +0200

    Analize the first snack with pseudo code

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -6,4 +6,12 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore. */
 
 
+// Analisi:
+//Creare un l'array di oggetti bici, ognuno con proprietà nome peso.
 
+//Impostare una variabile che possiamo chiamare 'biciLeggera' con il primo elemento
+
+// Scorrere tutte le bici:
+//  - SE ilpeso della bici corrente è minore del peso della biciLeggera, aggiorno biciLeggera
+
+// Stampare biciLeggera (nome e peso) in un cosole.log()
