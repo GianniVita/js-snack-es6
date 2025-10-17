493d20eade8a5b05d38053d0d7ebac4f4d10f07c
giannivitale <giavitale78@gmail.com>
2025-10-17 17:08:38 +0200

    Creating the new array and the function to generate random numbers

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -9,3 +9,21 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
+
+//Analisi:
+
+//Creare un array di oggetti di squadre, ogni oggetto ha nome, punti, "falliSubiti"
+const squadre = [
+    {nome:"AC Milan", punti: 0,fallisubiti: 0},
+    {nome:"Liverpool", punti: 0,fallisubiti: 0 },
+    {nome: "Atletico Madrid", punti: 0,fallisubiti: 0},
+    {nome:"Vasto Pro", punti: 0,fallisubiti: 0},
+    {nome:"Pescara", punti: 0,fallisubiti: 0},
+    {nome: "Napoli", punti: 0,fallisubiti: 0}
+];
+
+function randomNumber() {
+    return Math.floor(Math.random() * 99) + 1;
+    
+
+}
