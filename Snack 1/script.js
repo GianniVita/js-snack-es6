1dacf2b856ad17f5705ffb59fce1de21f9750e19
giannivitale <giavitale78@gmail.com>
2025-10-17 16:50:11 +0200

    Move the variable let  out of the function

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -9,36 +9,33 @@ Stampare a schermo la bici con peso minore. */
 // Analisi:
 //Creare un l'array di oggetti bici, ognuno con proprietà nome peso.
 const bici = [
-    { nome: "Pinarello Dogma",peso: 7.2 },
-    { nome: "Atala", peso: 10.1 },
-    { nome: "Bianchi Specialissima", peso: 7.8 },
-    { nome: "Colnago C64", peso: 6.9 },
-    { nome: "Treck Emonda", peso: 7.0 }
+    { nome: "Pinarello Dogma",peso: 7.1 },
+    { nome: "Atala", peso: 10.4 },
+    { nome: "Bianchi Specialissima", peso: 5.8 },
+    { nome: "Colnago C64", peso: 6.1 },
+    { nome: "Treck Emonda", peso: 8.2 }
 ];
+
+//Impostare una variabile che possiamo chiamare 'biciLeggera' con il primo elemento
+let biciLeggera = bici[0]; 
+
 //console.log(bici);
 // Controlliamo SE in caso l'array è vuoto possiamo dire un messaggio nel console.log
 if (bici.length === 0) {
     console.log("Non ci sono bici nell'array");
-} else {
-//Impostare una variabile che possiamo chiamare 'biciLeggera' con il primo elemento
-let biciLeggera = bici[0];    
+} else {  
 //console.log(biciLeggera);
 // Scorrere tutte le bici:
 for (let i = 0; i < bici.length; i++) {
     //  - SE il peso della bici corrente è minore del peso della biciLeggera, aggiorno biciLeggera
 let biciLeggera = bici[0];    
     if (bici[i].peso < biciLeggera.peso) {
-        
+        biciLeggera = bici[i]
     }
- 
-    
-
+ }
 }
-
-
-}
-
-
-
 
 // Stampare biciLeggera (nome e peso) in un cosole.log()
+console.log(biciLeggera);
+
+
